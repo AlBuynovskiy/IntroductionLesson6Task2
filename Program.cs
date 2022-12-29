db2837b1f08221507af57bb0bc991152ff0aaf4a
Aliaksandr Buinouski <AlBuynovskiy@gmail.com>
2022-12-29 16:37:08 +0300

    Program debugging 1

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@
 задаются пользователем.
 */
 
-Console.WriteLine("введите значение b1");
+Console.WriteLine("Введите значение b1: ");
 double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
+Console.WriteLine("Введите значение k1: ");
 double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
+Console.WriteLine("Введите значение b2: ");
 double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
+Console.WriteLine("Введите значение k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
 double x = (-b2 + b1)/(-k1 + k2);
 double y = k2 * x + b2;
 
-Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
